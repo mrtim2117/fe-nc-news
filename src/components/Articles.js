ec2e7cdd9a8353410a47c9d9d8a6be0fac91946a
mrtim2117 <tim.staton@outlook.com>
2022-01-17 12:39:50 +0000

    removed last of warnings

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./Articles.module.css";
 
