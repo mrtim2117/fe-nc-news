badf2b5f8d33508210cedf820bb2ef965cd82083
mrtim2117 <tim.staton@outlook.com>
2022-02-09 19:54:29 +0000

    removed left over comments

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -16,8 +16,6 @@ const Articles = () => {
   if (!loggedIn) navigate("/", { replace: true });
 
   const [articles, setArticles] = useState([]);
-  // import useSearchParams from "react-router-dom" to re-enable
-  // const [searchParams, setSearchParams] = useSearchParams();
 
   const [topic, setTopic] = useState();
   const [sort_by, setSortBy] = useState();
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ import homeimage from "./news.jpg";
 import styles from "./Home.module.css";
 
 const Home = () => {
-  //   return <div><img src={homeimage} alt="Home" /></div>
   return (
     <div className={styles.container}>
       <img src={homeimage} alt="Home" className={styles.image} />
