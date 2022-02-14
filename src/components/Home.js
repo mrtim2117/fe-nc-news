5079f3289cb30194d9425fec5220d16246844b17
mrtim2117 <tim.staton@outlook.com>
2022-02-14 21:51:40 +0000

    added more styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,6 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <img src={homeimage} alt="Home" className={styles.image} />
-      {/* <p>hello world</p> */}
     </div>
   );
 };
